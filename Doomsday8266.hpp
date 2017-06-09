a7aac4c9f422d66154c95a1fb8d82e4d3394f088
Paul Granov <granov@digisky.ru>
2017-06-09 17:21:19 +0300

    READY

--- Doomsday8266.hpp
+++ Doomsday8266.hpp
@@ -9,18 +9,69 @@ extern "C" {
 }
 #endif
 
+#include <ArduinoJson.h>
+#include "FS.h"
+
+
 
 typedef struct Config {
-  char* ssid;
-  char* pass;
-  char* pincode;
+  const char* ssid;
+  const char* pass;
+  const char* pincode;
 } Config;
+
+void loadsConfig(Config* cfg) {
+  File configFile = SPIFFS.open("/config.json", "r");
+  if (!configFile) {
+    Serial.println("Failed to open config file");
+    return ;
+  }
+
+  size_t size = configFile.size();
+  if (size > 1024) {
+    Serial.println("Config file size is too large");
+    return ;
+  }
+
+  // Allocate a buffer to store contents of the file.
+  std::unique_ptr<char[]> buf(new char[size]);
+
+  // We don't use String here because ArduinoJson library requires the input
+  // buffer to be mutable. If you don't use ArduinoJson, you may as well
+  // use configFile.readString instead.
+  configFile.readBytes(buf.get(), size);
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& json = jsonBuffer.parseObject(buf.get());
+
+  if (!json.success()) {
+    Serial.println("Failed to parse config file");
+    return ;
+  }
+
+  cfg->ssid = json["ssid"];
+  cfg->pass = json["pass"];
+  cfg->pincode = json["pincode"];
+  
+//  const char* serverName = json["serverName"];
+//  const char* accessToken = json["accessToken"];
+
+  // Real world application would store these values in some variables for
+  // later use.
+
+  Serial.print("Loaded serverName: ");
+//  Serial.println(serverName);
+  Serial.print("Loaded accessToken: ");
+//  Serial.println(accessToken);
+//  return true;
+}
+
 
 
 Config loadConfig() {
   Config cfg;
-  cfg.ssid = "DIGISKY";
-  cfg.pass = "D1G1onAir";
+  cfg.ssid = "TEST";
+  cfg.pass = "TEST";
   cfg.pincode = "12345678";
   return cfg;
 }
@@ -51,7 +102,14 @@ Esp* esp;
 Config cfg;
 void setup_prod() {
   digitalWrite(LED_BUILTIN, LOW);
+
+  if (!SPIFFS.begin()) {
+    Serial.println("Failed to mount file system");
+    return;    
+  }
+  
   cfg = loadConfig();
+  loadsConfig(&cfg);
   endpoint = new Endpoint();
   endpoint->setPin(cfg.pincode);  
   proto = new Protocol(endpoint, NULL);
